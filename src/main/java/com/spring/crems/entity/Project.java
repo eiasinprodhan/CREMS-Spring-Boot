0afc4b3919fbe3f29612b1ba5549981b8167ba1f
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-26 13:14:39 +0600

    Added

--- src/main/java/com/spring/crems/entity/Project.java
+++ src/main/java/com/spring/crems/entity/Project.java
@@ -2,7 +2,7 @@ package com.spring.crems.entity;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "projects")
@@ -11,13 +11,89 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String  name;
     private long budget;
-    private Date startDate;
-    private Date ExpectedEndDate;
+    private LocalDate startDate;
+    private LocalDate ExpectedEndDate;
     private String projectType;
-    private String projectManager;
+    private int projectManager;
     private String description;
 
+    public Project() {
+    }
+
+    public Project(int id, String name, long budget, LocalDate startDate, LocalDate expectedEndDate, String projectType, int projectManager, String description) {
+        this.id = id;
+        this.name = name;
+        this.budget = budget;
+        this.startDate = startDate;
+        ExpectedEndDate = expectedEndDate;
+        this.projectType = projectType;
+        this.projectManager = projectManager;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getBudget() {
+        return budget;
+    }
+
+    public void setBudget(long budget) {
+        this.budget = budget;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getExpectedEndDate() {
+        return ExpectedEndDate;
+    }
+
+    public void setExpectedEndDate(LocalDate expectedEndDate) {
+        ExpectedEndDate = expectedEndDate;
+    }
+
+    public String getProjectType() {
+        return projectType;
+    }
+
+    public void setProjectType(String projectType) {
+        this.projectType = projectType;
+    }
+
+    public int getProjectManager() {
+        return projectManager;
+    }
+
+    public void setProjectManager(int projectManager) {
+        this.projectManager = projectManager;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
