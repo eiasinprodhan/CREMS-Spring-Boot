9df56f45c68620116852eb603e3b8ad95346977e
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-07-26 11:34:17 +0600

    Added

---
+++ src/main/java/com/spring/crems/entity/Project.java
@@ -0,0 +1,23 @@
+package com.spring.crems.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String  name;
+    private long budget;
+    private Date startDate;
+    private Date ExpectedEndDate;
+    private String projectType;
+    private String projectManager;
+    private String description;
+
+}
